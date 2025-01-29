fbcab2b5e2220f4fc7f41d86ee518628222f887e
Nilesh Jagadale <124576357+Nilesh-Jagadale658@users.noreply.github.com>
2025-01-29 12:05:34 +0530

    Updated code

--- app/(auth)/(signin)/page.tsx
+++ app/(auth)/(signin)/page.tsx
@@ -1,76 +1,104 @@
-import { Metadata } from 'next';
-import dynamic from 'next/dynamic';
-import Link from 'next/link';
+"use client";
+
+import React, { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import Image from 'next/image';
-import { buttonVariants } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '@/components/ui/form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+import { useRouter } from 'next/navigation';
 import { cn } from '@/lib/utils';
 
-const UserAuthForm = dynamic(() => import('@/components/forms/user-auth-form'), { ssr: false });
+const formSchema = z.object({
+  aadharNumber: z.string().min(10, { message: 'Aadhar must be at least 10 characters long' })
+});
 
-export const metadata: Metadata = {
-  title: 'Authentication',
-  description: 'Authentication forms built using the components.'
-};
+type UserFormValue = z.infer<typeof formSchema>;
 
-export default function AuthenticationPage() {
+export default function SignUp() {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const [isToast, setToast] = useState(false);
+  const [message, setMessage] = useState('');
+  const [show, setShow] = useState(false);
+  const [type, setType] = useState<any>(null);
+  const defaultValues = {
+    aadharNumber: '',
+  };
+  const form = useForm<UserFormValue>({
+    resolver: zodResolver(formSchema),
+    defaultValues
+  });
+
+  const onSubmit = async (data: UserFormValue) => {
+    setLoading(true)
+    const obj = {
+      aadharNumber: data.aadharNumber
+    }
+    router.push('/aadharotp');
+    // const result = await login(obj);
+    // if (result.status === 200) {
+    //   router.push('/dashboard');
+    //   setToast(true);
+    //   setType('success')
+    //   setMessage('Login successfully!')
+    // } else {
+    //   setToast(true);
+    //   setType('error')
+    //   setMessage(result?.data?.message)
+    // }
+    setLoading(false)
+  };
+
   return (
-    <div className="relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-      <Link
-        href="/examples/authentication"
-        className={cn(
-          buttonVariants({ variant: 'ghost' }),
-          'absolute right-4 top-4 hidden md:right-8 md:top-8'
-        )}
-      >
-        Login
-      </Link>
-      <div className="relative hidden h-full flex-col bg-[url('/bg.svg')] bg-cover bg-center p-10 text-white lg:flex dark:border-r">
-        <div className="absolute inset-0 bg-[#0000001a]" />
-        <div className="relative z-20 flex items-center text-black text-2xl font-bold">
-          <Image src="/logo.png" width={200} height={200} alt="icon" className="mr-2 h-8 w-32" />
-        </div>
-        <div className="relative z-20 mt-auto rounded-lg -mb-4">
-          <p className="text-lg text-black p-2 font-semibold">
-            World’s first Evidence Based Generative AI for Orthopaedics built on LLM and Cognitive search model.
-          </p>
-        </div>
+    <div className="p-4 flex flex-col mt-40">
+      <div className="text-center text-gray-600">
+        To create a new store, complete your KYC by validating your Aadhaar number
       </div>
-      <div className="flex h-full items-center p-4 lg:p-8">
-        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-          <div className='flex justify-center'>
-            <Image src="/favicon.png" width={200} height={200} alt="icon" className="h-10 w-10" />
-          </div>
-          <div className="flex flex-col space-y-2 text-center">
-            <h1 className="text-base font-semibold tracking-tight mb-3 -mt-2">
-              Start analyzing smarter
-            </h1>
-            <h1 className="text-2xl font-semibold tracking-tight">
-              Sign in to Ortho Analytics
-            </h1>
-            <p className="text-sm text-muted-foreground">
-              Please enter your details below to sign in to your account
-            </p>
-          </div>
-          <UserAuthForm />
-          <p className="px-8 text-center text-sm text-muted-foreground">
-            By clicking continue, you agree to our{' '}
-            <Link
-              href="/terms"
-              className="underline underline-offset-4 hover:text-primary"
-            >
-              Terms of Service
-            </Link>{' '}
-            and{' '}
-            <Link
-              href="/privacy"
-              className="underline underline-offset-4 hover:text-primary"
-            >
-              Privacy Policy
-            </Link>
-            .
-          </p>
-        </div>
-      </div>
+
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col items-center gap-4 mt-16 w-full"
+        >
+          {/* Aadhaar Number Input */}
+          <FormField
+            control={form.control}
+            name="aadharNumber"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                {/* <FormLabel>Aadhaar Number</FormLabel> */}
+                <FormControl>
+                  <Input
+                    type="number"
+                    className="bg-card hover:border hover:border-slate-400 border-slate-300 w-full"
+                    placeholder="Enter your Aadhaar Number..."
+                    disabled={loading}
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          {/* Proceed Button */}
+          <Button
+            type="submit"
+            className="w-full bg-blue-500 text-white hover:bg-blue-600"
+          >
+            Proceed
+          </Button>
+        </form>
+      </Form>
     </div>
   );
 }
